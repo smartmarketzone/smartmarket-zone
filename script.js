f64f04ceaaf797167ade517a251a4bf87509a225
smartmarketzone <gabriel01cvenus@gmail.com>
2026-03-17 10:41:01 -0400

    Update map location and markers for Valencia area

--- script.js
+++ script.js
@@ -25,28 +25,34 @@ results.appendChild(li);
 
 }
 
-let map = L.map('map').setView([40.4168,-3.7038],13);
+let map = L.map('map').setView([10.17, -68.03], 9);
 
 L.tileLayer(
 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 {
-maxZoom:19
+maxZoom:18
 }
 ).addTo(map);
 
-L.marker([40.4168,-3.7038]).addTo(map)
-.bindPopup("Tienda Juan")
-.openPopup();
+// Valencia
+L.marker([10.162, -68.007]).addTo(map)
+.bindPopup("Valencia - Capital de Carabobo");
 
-L.marker([40.4200,-3.7050]).addTo(map)
-.bindPopup("Market Ana");
+// Puerto Cabello
+L.marker([10.475, -68.012]).addTo(map)
+.bindPopup("Puerto Cabello");
 
-document
-.getElementById("storeForm")
-.addEventListener("submit",function(e){
+// Naguanagua
+L.marker([10.26, -68.01]).addTo(map)
+.bindPopup("Naguanagua");
 
-e.preventDefault();
+// Guacara
+L.marker([10.23, -67.88]).addTo(map)
+.bindPopup("Guacara");
 
-alert("Comercio registrado (simulación)");
+let bounds = [
+[9.77, -68.56],
+[10.66, -67.39]
+];
 
-});
+map.setMaxBounds(bounds);
