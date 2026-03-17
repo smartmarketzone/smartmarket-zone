13eb89b8030c9197398fb68dd71f9004bbba4d56
smartmarketzone <gabriel01cvenus@gmail.com>
2026-03-17 18:36:52 -0400

    Add Firebase authentication functions and config

---
+++ auth.js
@@ -0,0 +1,43 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6tYooh4CtX7ww9GRyXWhuakl2nLovtsY",
+  authDomain: "smartmarket-zone-e7559.firebaseapp.com",
+  projectId: "smartmarket-zone-e7559",
+};
+
+// INICIALIZAR
+firebase.initializeApp(firebaseConfig);
+
+// CAMBIO DE VISTAS
+function showLogin(){
+document.getElementById("loginBox").style.display="block";
+document.getElementById("registerBox").style.display="none";
+}
+
+function showRegister(){
+document.getElementById("loginBox").style.display="none";
+document.getElementById("registerBox").style.display="block";
+}
+
+// REGISTRO
+function register(){
+
+let email = document.getElementById("regEmail").value;
+let password = document.getElementById("regPass").value;
+
+firebase.auth().createUserWithEmailAndPassword(email, password)
+.then(() => alert("Cuenta creada correctamente"))
+.catch(e => alert(e.message));
+
+}
+
+// LOGIN
+function login(){
+
+let email = document.getElementById("loginEmail").value;
+let password = document.getElementById("loginPass").value;
+
+firebase.auth().signInWithEmailAndPassword(email, password)
+.then(() => alert("Bienvenido"))
+.catch(e => alert(e.message));
+
+}
