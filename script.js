100ac893e3f1565decc8e0d7f9e1dbbd6c473a2a
smartmarketzone <gabriel01cvenus@gmail.com>
2026-03-17 11:12:26 -0400

    Refactor map initialization and marker setup

--- script.js
+++ script.js
@@ -1,35 +1,8 @@
-function searchProduct(){
-
-// MAPA CARABOBO LIMPIO
-
 let map = L.map('map').setView([10.17, -68.03], 9);
 
-L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
-maxZoom: 18
-}
-).addTo(map);
-
-// MARCADORES
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+maxZoom:18
+}).addTo(map);
 
 L.marker([10.162, -68.007]).addTo(map)
-.bindPopup("Valencia - Capital de Carabobo");
-
-L.marker([10.475, -68.012]).addTo(map)
-.bindPopup("Puerto Cabello");
-
-L.marker([10.26, -68.01]).addTo(map)
-.bindPopup("Naguanagua");
-
-L.marker([10.23, -67.88]).addTo(map)
-.bindPopup("Guacara");
-
-// LIMITES
-
-let bounds = [
-[9.77, -68.56],
-[10.66, -67.39]
-];
-
-map.setMaxBounds(bounds);
+.bindPopup("Valencia");
