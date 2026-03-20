aeb96c2c005c6d0dcabb17797562ee8aa8e1ca1d
smartmarketzone <gabriel01cvenus@gmail.com>
2026-03-20 13:24:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-let map = L.map('map').setView([10.17, -68.03], 9);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-maxZoom:18
-}).addTo(map);
-
-L.marker([10.162, -68.007]).addTo(map)
-.bindPopup("Valencia");
